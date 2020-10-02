6ecd6e488d3626d5aa5a74d83eaa6b2b34fe6efd
AustinKavelman <kavea9410@wrdsb.ca>
2020-10-02 16:21:28 +0000

    Finished a4q3

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * 
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +11,19 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+    //create a variable to keep of what number we are on
+    int count = 1;
+    //the accumilator to add the count info
+    int total = 0;
+    while(count <= 10){
+    //add the numbers as we go into total
+    total = total + count;
+    //add 1 to our count to increase int0
+    count = count + 1;
+    }
+    //print the total to the screen
+    System.out.println("The total is " + total);
   }
 }
